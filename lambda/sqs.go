ec29bb1f51f67520ed59bee3aa20e9a45e6bfef7
Alexandre Leal <alexandre.leal@b2wdigital.com>
2022-05-10 16:47:43 -0300

    fix: sqs wraps sns

--- lambda/sqs.go
+++ lambda/sqs.go
@@ -2,7 +2,6 @@ package lambda
 
 import (
 	"encoding/json"
-	"strings"
 
 	"github.com/americanas-go/errors"
 	"github.com/aws/aws-lambda-go/events"
@@ -11,43 +10,19 @@ import (
 )
 
 func fromSQS(record Record) (*event.Event, error) {
-	var err error
-
 	in := v2.NewEvent()
 	body := []byte(record.Body)
-	if err = json.Unmarshal(body, &in); err != nil {
-		var data interface{}
-		if err = json.Unmarshal(body, &data); err != nil {
-			return nil, errors.New("not json message on SQS record")
-		} else if isSNSMessage(body) {
-			if err = unmarshalSNSMessage(body, &data); err != nil {
-				return nil, err
-			}
+	if err := json.Unmarshal(body, &in); err != nil {
+		snsEvent := events.SNSEntity{}
+		if err = json.Unmarshal(body, &snsEvent); err == nil && snsEvent.Message != "" {
+			body = []byte(snsEvent.Message)
 		}
-		if err = in.SetData(v2.ApplicationJSON, data); err != nil {
+		if err = in.SetData(v2.ApplicationJSON, body); err != nil {
 			return nil, errors.NewNotValid(err, "could not set data in event")
 		}
 	}
 	if in.ID() == "" {
 		in.SetID(record.MessageId)
 	}
-	return &in, err
-}
-
-func isSNSMessage(data []byte) bool {
-	s := string(data)
-	return strings.Contains(s, "\"TopicArn\":") && strings.Contains(s, "\"Message\":")
-}
-
-func unmarshalSNSMessage(body []byte, data *interface{}) (err error) {
-	snsEvent := events.SNSEntity{}
-	if err = json.Unmarshal(body, &snsEvent); err != nil {
-		return errors.NewNotValid(err, "unrecognized message on SQS record")
-	}
-	if snsEvent.Message != "" {
-		if er := json.Unmarshal([]byte(snsEvent.Message), data); er != nil {
-			err = errors.NewNotValid(er, "SNS message is not a json")
-		}
-	}
-	return
+	return &in, nil
 }
